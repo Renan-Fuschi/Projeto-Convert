6ac1e13f03a69c0016e36204c5790a4ea33ac026
Renan Fuschi <renanfuschi@gmail.com>
2025-11-27 16:41:09 -0300

    Convert: Captura o valor do evento de submit do formulário

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 const amount = document.getElementById("amount")
+const form = document.querySelector("form")
+const currency = document.getElementById("currency")
 
 // Manipulando o input amount para receber somente números
 amount.addEventListener("input", (event) => {
@@ -6,8 +8,16 @@ amount.addEventListener("input", (event) => {
 
     const onlyNumberRegex = /^\d+$/
     const hasCharacterRegex = /[^0-9]/g
-    
+
     if(!onlyNumberRegex.test(amount.value)) {
         amount.value = amount.value.replace(hasCharacterRegex, "")
     }
 })
+
+// Captando o evento de submit do formulário
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    console.log(currency.value)
+    
+}
