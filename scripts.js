bcfa5eefb04d8875fcefd9d47da26a4ee1b8f49d
Renan Fuschi <renanfuschi@gmail.com>
2025-11-27 17:02:57 -0300

    Convert: Adiciona tipos de moedas e seus respectivos valores atuais, aplica um switch e um bloco try-catch

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
+const USD = 5.36
+const EUR = 6.21
+const GBP = 7.09
+
+
 const amount = document.getElementById("amount")
 const form = document.querySelector("form")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
 
 // Manipulando o input amount para receber somente números
 amount.addEventListener("input", (event) => {
@@ -18,6 +26,29 @@ amount.addEventListener("input", (event) => {
 form.onsubmit = (event) => {
     event.preventDefault()
 
-    console.log(currency.value)
-    
+    const currencyValue = currency.value
+    const amountValue = amount.value
+
+    switch (currency.value) {
+        case "USD":
+            convertCurrency(amountValue, USD, "US$")
+            break
+        case "EUR":
+            convertCurrency(amountValue, EUR, "€")
+            break
+        case "GBP":
+            convertCurrency(amountValue, GBP, "£")
+            break
+    }
 }
+
+
+    function convertCurrency(amountValue, currencyValue, symbol) {
+        try {
+            footer.classList.add("show-result")
+        } catch (error) {
+            console.log(error)
+            footer.classList.remove("show-result")
+            alert("Erro ao converter moeda, tente novamente.")
+        }
+    }
